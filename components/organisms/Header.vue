98930197b23aada260093e70dd2d6b51ac65fa12
paulabssantos <paulablsantos7@gmail.com>
2023-09-24 21:18:16 -0400

    feat: cria sidebar e header

---
+++ components/organisms/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <Logo></Logo>
+    <div class="items">
+      <img
+        class="item"
+        src="~/assets/baseline-notifications-24px.svg"
+        width="24"
+        height="24"
+      />
+      <p class="item">Administrador</p>
+      <img
+        class="item"
+        src="~/assets/baseline-exit_to_app-24px.svg"
+        width="24"
+        height="24"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import Logo from '~/components/atoms/Logo.vue'; 
+export default {
+  components: {
+    Logo
+  },
+};
+</script>
+<style scoped>
+
+.header {
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  background: #fbfbfb;
+  opacity: 1;
+}
+
+.items {
+  display: flex;
+  align-items: center;
+}
+
+.item {
+  margin-right: 20px;
+}
+
+.items p {
+  text-align: left;
+  font: normal normal normal 15px/20px Roboto;
+  color: #000000;
+}
+</style>
---
+++ components/organisms/SideBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="sidebar">
+    <nuxt-link to="/">
+      <img
+        class="item"
+        src="~/assets/baseline-home-24px.svg"
+        width="24"
+        height="24"
+      />
+    </nuxt-link>
+    <img
+      class="item"
+      src="~/assets/baseline-note_add-24px.svg"
+      width="24"
+      height="24"
+    />
+    <img
+      class="item"
+      src="~/assets/baseline-description-24px.svg"
+      width="24"
+      height="24"
+    />
+    <img
+      class="item"
+      src="~/assets/assignment_turned_in-24px.svg"
+      width="24"
+      height="24"
+    />
+    <img
+      class="item"
+      src="~/assets/baseline-assignment-24px.svg"
+      width="24"
+      height="24"
+    />
+    <img
+      class="item"
+      src="~/assets/baseline-file_copy-24px.svg"
+      width="24"
+      height="24"
+    />
+  </div>
+</template>
+<style scoped>
+.sidebar {
+  width: 66px;
+  background: #606062;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.sidebar img {
+  margin-top: 50px;
+}
+</style>
