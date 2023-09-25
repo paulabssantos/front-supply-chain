041431aba4c90b62883bad4628bc969799ec272b
paulabssantos <paulablsantos7@gmail.com>
2023-09-24 21:17:59 -0400

    feat: cria layout default

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <Header />
+    <div class="sidebar">
+      <Sidebar />
+      <div class="content">
+        <Nuxt />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/organisms/Header.vue"; // Importe o componente Header
+import Sidebar from "~/components/organisms/SideBar.vue"; // Importe o componente Sidebar
+
+export default {
+  components: {
+    Header,
+    Sidebar,
+  },
+};
+</script>
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+}
+
+.sidebar {
+  display: flex;
+}
+
+.content {
+  width: 100%;
+}
+</style>
